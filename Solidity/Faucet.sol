d0ea9f75fc6b8980046e88a96c85c5eba9220f9a
Herbert Lago <hebertpdl@gmail.com>
2022-01-31 07:59:29 -0300

    feat(Faucet): add constructor and destructor methods

--- Solidity/Faucet.sol
+++ Solidity/Faucet.sol
@@ -3,13 +3,30 @@
 pragma solidity ^0.8.11;
 
 contract Faucet {
+  address payable owner;
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  // Initialize Faucet contract: set owner
+  constructor() {
+    owner = payable(msg.sender);
+  }
+
+  // Contract destructor
+  function destroy() public onlyOwner {
+    selfdestruct(owner);
+  }
+
   // Accept any incoming amount
   receive() external payable{}
 
   // Give out ether to anyone who asks
   function withdraw(uint withdraw_amount) public {
     // Limit withdraw amount
-    require(withdraw_amount <= 100000000000000000);
+    require(withdraw_amount <= 0.1 ether);
 
     // Send the amount to the address that requiested it
     payable(msg.sender).transfer(withdraw_amount);
